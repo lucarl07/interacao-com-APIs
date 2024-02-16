8a9a44766349181185fe6235e9e803bad27652f3
lucarl07 <junitinho2007@gmail.com>
2024-02-16 10:01:45 -0300

    Finalizar o código do projeto01

--- programacao_assync/projeto01/js/app.js
+++ programacao_assync/projeto01/js/app.js
@@ -10,28 +10,31 @@ document.addEventListener('DOMContentLoaded', function() {
         return response.json()
     })
     .then((data) => {
-        let listCategories = '=:=:=:= CATEGORIES =:=:=:='
-
-        data.forEach((category, index) => {
-            if(index % 2 == 0) {
-                listCategories += `\n${index+1}. ${category}`
-
-                if(category.length < 5) {
-                    listCategories += `\t`  
-                }
-            } else {
-                listCategories += `\t${index+1}. ${category}`
-            }
-        })
-        console.log(listCategories)
-
-        gerarCategorias(data)
+        listCategories(data)
+        getCategories(data)
     })
-    .catch((err) => console.log(err))
+    .catch((err) => console.error(err))
 
 })
 
-function gerarCategorias(categories) {
+function listCategories(array) {
+    let list = '=:=:=:= CATEGORIES =:=:=:='
+
+    array.forEach((category, index) => {
+        if(index % 2 == 0) {
+            list += `\n${index+1}. ${category}`
+
+            if(category.length < 5) {
+                list += `\t`  
+            }
+        } else {
+            list += `\t${index+1}. ${category}`
+        }
+    })
+    console.log(list)
+}
+
+function getCategories(categories) {
 
     const select = document.querySelector('#chooseCtg')
     
@@ -39,8 +42,30 @@ function gerarCategorias(categories) {
         const options = document.createElement('option')
         
         options.innerHTML = `${category}`
+        options.value = category
         select.appendChild(options)
     })
 
 }
 
+const btn = document.querySelector('#btnGetJoke')
+
+btn.addEventListener('click', function() {
+
+    const select = document.querySelector('#chooseCtg').value,
+    url = `https://api.chucknorris.io/jokes/random?category=${select}`
+
+    fetch(url).then(response => {
+        if(!response.ok) {
+            throw new Error('Erro ao receber os dados.')
+        }
+
+        return response.json()
+    })
+    .then(data => getJoke(data))
+    .catch(err => console.error(err))
+})
+
+function getJoke(joke) {
+    document.querySelector('#joke').innerHTML = joke.value
+}
